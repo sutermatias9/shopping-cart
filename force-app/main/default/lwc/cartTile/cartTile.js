86c8d32132256da27b25e11c8c224fea2ff3a530
Matias Suter <sutermatias9@gmail.com>
2024-07-03 01:38:50 -0300

    price and quantity

--- force-app/main/default/lwc/cartTile/cartTile.js
+++ force-app/main/default/lwc/cartTile/cartTile.js
@@ -2,4 +2,17 @@ import { LightningElement, api } from 'lwc';
 
 export default class CartTile extends LightningElement {
     @api item;
+    quantity;
+
+    get price() {
+        return (this.quantity * this.item.Unit_Price__c).toFixed(2);
+    }
+
+    connectedCallback() {
+        this.quantity = this.item.Quantity__c;
+    }
+
+    handleCounterChange(event) {
+        this.quantity = event.detail;
+    }
 }
