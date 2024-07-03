6faa3b7344088956f996c829e6a24529a42bd595
Matias Suter <sutermatias9@gmail.com>
2024-07-03 20:12:02 -0300

    Create order

--- force-app/main/default/lwc/cart/cart.js
+++ force-app/main/default/lwc/cart/cart.js
@@ -1,10 +1,21 @@
 import { LightningElement, api } from 'lwc';
+import userId from '@salesforce/user/Id';
+import createOrder from '@salesforce/apex/OrderHandler.createOrder';
+import setCartAsPurchased from '@salesforce/apex/CartHandler.setAsPurchased';
 
 export default class Cart extends LightningElement {
     @api cart;
+    isCreating = false;
 
     handleCancelClick() {
         this.fireCancelEvent();
+    }
+
+    async handleCreateClick() {
+        this.isCreating = true;
+        await createOrder({ userId });
+        await setCartAsPurchased({ userId });
+        this.isCreating = false;
     }
 
     fireCancelEvent() {
