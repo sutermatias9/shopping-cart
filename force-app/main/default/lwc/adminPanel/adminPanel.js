7abf5fe5b722fa889ea60fbb72e19506e158b2be
Matias Suter <sutermatias9@gmail.com>
2024-09-29 21:02:31 -0300

    Get user carts on component load

--- force-app/main/default/lwc/adminPanel/adminPanel.js
+++ force-app/main/default/lwc/adminPanel/adminPanel.js
@@ -1,3 +1,47 @@
 import { LightningElement } from 'lwc';
+import getAllCarts from '@salesforce/apex/CartHandler.getAllCarts';
 
-export default class AdminPanel extends LightningElement {}
+export default class AdminPanel extends LightningElement {
+    userCarts;
+
+    connectedCallback() {
+        this.loadCarts();
+        console.log(JSON.stringify(this.userCarts));
+    }
+
+    async loadCarts() {
+        const cartsByUser = await getAllCarts();
+
+        this.userCarts = Object.keys(cartsByUser).map((userName) => {
+            return {
+                userName,
+                currentCart: this.getCurrentCartInfo(cartsByUser[userName].find((cart) => cart.Status__c === 'Active')),
+                totalSpent: this.getPurchasedCartsTotalAmount(cartsByUser[userName].filter((cart) => cart.Status__c === 'Purchased'))
+            };
+        });
+    }
+
+    getCurrentCartInfo(cart) {
+        const info = { numberOfItems: 0, totalAmount: 0 };
+
+        if (cart) {
+            info.numberOfItems = cart.Cart_Items__r.length;
+            info.totalAmount = this.getCartTotal(cart);
+        }
+
+        return info;
+    }
+
+    getPurchasedCartsTotalAmount(purchasedCarts) {
+        return purchasedCarts.reduce((accumulator, cart) => {
+            return accumulator + parseFloat(this.getCartTotal(cart));
+        }, 0);
+    }
+
+    getCartTotal(cart) {
+        const sum = cart.Cart_Items__r.reduce((accumulator, item) => {
+            return accumulator + item.Unit_Price__c;
+        }, 0);
+        return sum.toFixed(2);
+    }
+}
